793c1cda18ad1ba9c589cf2cbbfb99e658535b9b
Lior3796 <liorlsa9@gmail.com>
2021-08-18 17:43:52 +0300

    adding eslint file and add dependencies

--- server/index.js
+++ server/index.js
@@ -1,10 +1,8 @@
-const express;
-const yoni = "yoni";
-const lior = "lior 10 "
-const hana = "Karhana";
+const yoni = 'yoni';
+const lior = 'lior 10 ';
+const hana = 'Karhana';
 
-const name="michal";
-const server = "server12";
+const name = 'michal';
+const server = 'server12';
 
-const name="michal 2"
-
+const name1 = 'michal 2 ';
