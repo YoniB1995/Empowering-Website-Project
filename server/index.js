23b71700eb88ca45fa88d482c5484976737630be
asalef10 <76787292+asalef10@users.noreply.github.com>
2021-08-18 15:28:13 +0300

    Asi branch (#19)
    * asii
    
    * assi
    
    * l
    
    * changa my name
    
    Co-authored-by: YehudaBayana <76784836+YehudaBayana@users.noreply.github.com>

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const yoni = "yoni";
 const lior = "lior 10 "
 const hana = "Karhana";
 const name="michal"
+const name="asi alena"
 const fname= "beniy"
 
 const name="michal";
@@ -11,4 +12,3 @@ const server = "server12";
 const name="michal 2"
 
 const name = "Nati Dese 1";
-
