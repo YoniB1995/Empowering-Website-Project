3a1f72b325b05040203d329f5f823f5cf3a82658
Yoni <yonatansamfisher@gmail.com>
2021-08-18 17:03:55 +0300

    added

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const yoni = "yoni";
 const lior = "lior 10 "
 const hana = "Karhana";
 const branch = "lior branch"
+<<<<<<< HEAD
+=======
+
+>>>>>>> 43050392ff393de5d7abdb3b27e13959a851ed1a
 const name="michal"
 const hana="hanaA";
 const name="asi alena"
