7e7fcbe9b5edf5d4f2d3a42f5d354e4be092e3d6
HanaAmara <hhanaamara@gmail.com>
2021-08-21 23:39:49 +0300

    modelling of valid fixed

--- server/validation/adminValidation.js
+++ server/validation/adminValidation.js
@@ -2,8 +2,8 @@ const joi = require('joi');
 
 exports.validContactUs = (bodyData) => {
   const joiSchema = joi.object({
-    password: joi.String().joi.min().joi.max().joi.require(),
-    email: joi.String().joi.min().joi.max().joi.require(),
+    password: joi.string().min().max().require(),
+    email: joi.string().min().max().require(),
   });
   return joiSchema.validate(bodyData);
 };
--- server/validation/contactUsValidation.js
+++ server/validation/contactUsValidation.js
@@ -2,9 +2,9 @@ const joi = require('joi');
 
 exports.validContactUs = (bodyData) => {
   const joiSchema = joi.object({
-    email: joi.String().joi.min().joi.max().joi.require(),
-    inquiry: joi.String().joi.min().joi.max().joi.require(),
-    content: joi.String().joi.min().joi.max().joi.require(),
+    email: joi.string().min().max().require(),
+    inquiry: joi.string().min().max().require(),
+    content: joi.string().min().max().require(),
   });
   return joiSchema.validate(bodyData);
 };
