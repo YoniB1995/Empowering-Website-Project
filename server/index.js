de47a77fe8ac7d93acfe9c6e3a195dbe74f3ed43
Lior3796 <liorlsa9@gmail.com>
2021-08-22 19:29:41 +0300

    change name of app in package JS

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
-const express;
-const yoni = "yoni";
-const lior = "lior 9 "
-const hana = "Karhana";
-const name="michal";
-const yuda = "for beni";
+const yoni = 'yoni';
+const lior = 'lior 9 ';
+const hana = 'Karhana';
+const name = 'michal';
+const yuda = 'for beni';
