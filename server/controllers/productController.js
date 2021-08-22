f59adfd05a324206b1bd5d247d862007bc03daf0
Lior3796 <liorlsa9@gmail.com>
2021-08-23 00:42:13 +0300

    added

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,9 +1,10 @@
 const productModel = require('../models/productModel');
+const productInfo = require('../data/productInfo.json');
 
 const getAllProducts = async (req, res) => {
   try {
     const products = await productModel.find({});
-    res.status(200).json({ productsList: products });
+    res.status(200).json('add to db');
   } catch (e) {
     console.log('cannot find list of products');
     res.status(400).json({ error: 'server error' });
@@ -12,7 +13,7 @@ const getAllProducts = async (req, res) => {
 
 const createProduct = async (req, res) => {
   try {
-    const products = await productModel.insertMany([{}]);
+    const products = await productModel.insertMany(productInfo);
     res.status(200).json({ productsList: products });
   } catch (e) {
     console.log('cannot find list of products');
