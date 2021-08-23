7178cfd6b2f0a9cf2e8751b40ffb095e031367fc
Asalef <asalef10@gmail.com>
2021-08-23 22:50:11 +0300

    add add joi admin auth,change admin controler,change admin routh #52

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,6 +1,6 @@
 const keys = {
-    mongoURI: "YOUR_MONGOURI_HERE", // env להשתמש ב
-    secretOrKey: "secret" // env להשתמש ב
-  };
-  
-module.exports =keys
+  mongoURI: 'YOUR_MONGOURI_HERE', // env להשתמש ב
+  secretOrKey: 'secret', // env להשתמש ב
+};
+
+module.exports = keys;
