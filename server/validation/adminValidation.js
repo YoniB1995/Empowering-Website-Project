7cdcba6669b3b5f9e6a7a29529b85c2d5e4c799e
Lior3796 <liorlsa9@gmail.com>
2021-08-25 09:42:32 +0300

    merge asi/auth branch

--- server/validation/adminValidation.js
+++ server/validation/adminValidation.js
@@ -1,19 +1,10 @@
 const joi = require('joi');
 
-<<<<<<< HEAD
-const adminSchema = require('../models/adminModel');
-
-=======
->>>>>>> asi/auth
 exports.validAdmin = (adminData) => {
   const joiSchema = joi.object({
     password: joi.string().min(2).max(99).required(),
     email: joi.string().min(4).max(99).required()
       .email(),
   });
-<<<<<<< HEAD
-  return joiSchema.validate(adminData, adminSchema);
-=======
   return joiSchema.validate(adminData);
->>>>>>> asi/auth
 };
