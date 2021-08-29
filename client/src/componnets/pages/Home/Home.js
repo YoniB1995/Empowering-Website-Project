8514717cf818dc28ab6cbcc4fc6d4d8bfb3dd2ee
michal212 <michalfarada1997@gmail.com>
2021-08-30 01:44:12 +0300

    ordeing home page

--- client/src/componnets/pages/Home/Home.js
+++ client/src/componnets/pages/Home/Home.js
@@ -1,28 +1,16 @@
 import React from "react";
-<<<<<<< HEAD
 import "./Home.css";
 import Button from "../../features/Button/Button";
 import GoalsCards from "./Goals/GoalsCards";
 import Copartners from "./Copartners/Copartners";
-
-const Home = () => {
-    return (
-        <div>
-        </div>
-    )
-}
-export default Home;
-=======
-import ImageSlider from "../../features/Slider/ImageSlider";
 import Card from "../../features/card/Card";
 import WorkersCards from "../../features/workersCards/WorkersCards";
 import Footer from "../../features/Footer/Footer";
+import ImageSlider from "../../features/Slider/ImageSlider";
 import "./Home.css";
 import "animate.css";
-import Button from "../../features/Button/Button";
 import Vision from "./vision/Vision";
 import { FaArrowDown } from "react-icons/fa";
-
 import "animate.css";
 const Home = () => {
   return (
@@ -56,38 +44,8 @@ const Home = () => {
         {/* <WorkersCards /> */}
       </div>
       <Vision />
-
-      {/* <div className="hazon">
-        <img
-          src="./eye.png"
-          style={{
-            width: "150px",
-            position: "absolute",
-            top: "-80px",
-            left: "auto",
-            right: "auto",
-          }}
-        />
-        z<h1> החזון שלנו</h1>
-        <h3 style={{ lineHeight: "1.8" }}>
-          הקבוצה הוקמה למען מטרה חשובה ונעלה שאנחנו הנשים האתיופיות החזקות,
-          האצילות והמוצלחות(למרות שלפעמים איננו מעריכות מספיק את עצמנו) ויש
-          בידינו הזדמנות לעזור, לפרגן, ללמוד זו מזו, להעשיר אחת את השנייה
-          ולהצליח יחד. האמונה שלי היא כי החוכמה שבנו(כחוכמת מלכת שבא) יכולה
-          לסייע בהעלאת נושאים חברתיים ואישיים הקשורים לעדה ובעיקר יכולה להביא
-          למינוף והצלחה של כל אחת ואחת.
-        </h3>
-        {/* <hr /> */}
-      {/* <Button
-          type="button"
-          text="לקרוא עוד"
-          className="myButton"
-          class="animate__animated animate__fadeIn"
-        />
-      </div> */}
     </div>
   );
 };
 
 export default Home;
->>>>>>> 729ffe675b0a32eb8816822f43b17e16f7dbf71c
