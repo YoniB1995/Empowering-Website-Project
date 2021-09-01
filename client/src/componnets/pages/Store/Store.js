22bdb315f0d06cc144d40458f2b6e37a8e9fc65a
eliasadmaso1 <76824469+eliasadmaso1@users.noreply.github.com>
2021-09-01 17:02:25 +0300

    service-update

--- client/src/componnets/pages/Store/Store.js
+++ client/src/componnets/pages/Store/Store.js
@@ -1,30 +1,31 @@
-import {useEffect,useState} from "react"
+import { useEffect, useState } from "react";
 import "./Store.css";
-import Card from '../../features/card/Card';
+import Card from '../../features/card/Card'
 
 const API =
   process.env.NODE_ENV === "production"
     ? `https://yonib.herokuapp.com`
     : "http://localhost:5000";
 
-const Store = ()=>{
-
+const Store = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     fetch(`http://localhost:5000/product`)
       .then((res) => res.json())
-      .then(result => setProducts(result));
+      .then((result) => {
+        setProducts(result.products);
+      });
   }, []);
 
-    return(
-        <div className="store-div">
-            {products.map((product)=>{
-            <Card title={product.title} icon={product.descripation}/>
-        })}
-
-        </div>
-    )
-}
+  console.log("proudcts: ", products);
+  return (
+    <div className="store-div">
+      {products.map((product) => {
+        return <Card icon={product.price} title={product.title} />;
+      })}
+    </div>
+  );
+};
 
 export default Store;
--- server/controllers/articleController.js
+++ server/controllers/articleController.js
@@ -1,46 +1,37 @@
-<<<<<<< HEAD
 /* eslint-disable new-cap */
-const ArticleModel = require('../models/article');
-=======
-/* eslint-disable no-underscore-dangle */
 /* eslint-disable consistent-return */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/extensions */
 /* eslint-disable new-cap */
-const articleModel = require('../models/articleModel');
-const ErrorResponse = require('../utils/errorResponse');
->>>>>>> 729ffe675b0a32eb8816822f43b17e16f7dbf71c
+
+const articleModel = require("../models/articleModel");
+const ErrorResponse = require("../utils/errorResponse");
 
 const createNewArticle = async (req, res, next) => {
   req.article = new articleModel();
   try {
-    res.render('articles/new', { article: new articleModel() });
+    res.render("articles/new", { article: new articleModel() });
   } catch (error) {
     console.log(error);
-    return next(new ErrorResponse('Server Error', 500));
+    return next(new ErrorResponse("Server Error", 500));
   }
   next();
 };
-<<<<<<< HEAD
-const getAllArticles = async (req, res) => {
-=======
 
 const getAllArticles = async (req, res, next) => {
   const article = await articleModel.find({});
   if (!article) {
-    return next(new ErrorResponse('Please provide article details to publish', 400));
+    return next(
+      new ErrorResponse("Please provide article details to publish", 400)
+    );
   }
->>>>>>> 729ffe675b0a32eb8816822f43b17e16f7dbf71c
   try {
-    res.render('articles/new', { article });
+    res.render("articles/new", { article });
   } catch (error) {
     console.log(error);
-    return next(new ErrorResponse('Server Error', 500));
+    return next(new ErrorResponse("Server Error", 500));
   }
 };
-<<<<<<< HEAD
-const editArticle = async (req, res) => {
-=======
 
 const editArticle = async (req, res, next) => {
   const oldArticle = await new articleModel({
@@ -49,38 +40,43 @@ const editArticle = async (req, res, next) => {
     description: req.body.description,
     markdown: req.body.markdown,
   });
-  if (!oldArticle._id || !oldArticle.title || !oldArticle.description || !oldArticle.markdown) {
-    return next(new ErrorResponse('Please provide article details to publish', 400));
+  if (
+    !oldArticle._id ||
+    !oldArticle.title ||
+    !oldArticle.description ||
+    !oldArticle.markdown
+  ) {
+    return next(
+      new ErrorResponse("Please provide article details to publish", 400)
+    );
   }
-  articleModel.updateOne({ _id: req.params.id }, oldArticle).then(
-    () => {
+  articleModel
+    .updateOne({ _id: req.params.id }, oldArticle)
+    .then(() => {
       res.status(201).json({
-        message: 'Thing updated successfully!',
+        message: "Thing updated successfully!",
       });
-    },
-  ).catch(
-    (error) => {
+    })
+    .catch((error) => {
       res.status(400).json({
         error,
       });
-    },
-  );
->>>>>>> 729ffe675b0a32eb8816822f43b17e16f7dbf71c
+    });
   try {
     const article = await articleModel.findById(req.params.id);
-    res.render('articles/edit', { article });
+    res.render("articles/edit", { article });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: 'Server Error' });
+    res.status(500).json({ message: "Server Error" });
   }
 };
 const deleteArticle = async (req, res) => {
   try {
     await articleModel.findByIdAndDelete(req.params.id);
-    res.redirect('/');
+    res.redirect("/");
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: 'Server Error' });
+    res.status(500).json({ message: "Server Error" });
   }
 };
 
