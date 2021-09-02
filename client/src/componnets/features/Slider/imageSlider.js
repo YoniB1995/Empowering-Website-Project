3430160be3619f64531bc61076863cb826be2c85
Lior3796 <liorlsa9@gmail.com>
2021-09-02 20:56:38 +0300

    add a alt attribute to img element

--- client/src/componnets/features/Slider/imageSlider.js
+++ client/src/componnets/features/Slider/imageSlider.js
@@ -1,22 +1,27 @@
 import React from "react";
+import "../Slider/slider.css";
 import ImgSliderData from "./imgSliderData";
 import { Carousel } from "antd";
 import "antd/dist/antd.css";
-import "../Slider/slider.css";
-
 
 const ImageSlider = () => {
-  return (
-    <div className="slider-img-section" style={{ paddingTop: "20px" }}>
-      <div className="slider-container">
-        <div className="container-frame-cube"></div>
-        <Carousel autoplay>
-          {ImgSliderData.map((slide) => (
-            <img src={slide.image} id="img-slide" />
-          ))}
-        </Carousel>
-      </div>
-    </div>
-  );
+	return (
+		<div className="slider-img-section" style={{ paddingTop: "20px" }}>
+			<div className="slider-container">
+				<div className="container-frame-cube"></div>
+				<Carousel autoplay>
+					{ImgSliderData.map((slide) => (
+						<div>
+							<img
+								src="https://images.pexels.com/photos/1010079/pexels-photo-1010079.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+								id="img-slide"
+								alt=""
+							/>
+						</div>
+					))}
+				</Carousel>
+			</div>
+		</div>
+	);
 };
 export default ImageSlider;
