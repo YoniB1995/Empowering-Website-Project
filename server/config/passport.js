99900d3940fee034fe7177c0dd23f3fd76977a07
michal212 <michalfarada1997@gmail.com>
2021-09-04 18:06:14 +0300

    Updating Heroku Deployment

--- server/config/passport.js
+++
@@ -1,5 +0,0 @@
-const keys = {
-  mongoURI: 'YOUR_MONGOURI_HERE', // env להשתמש ב
-  secretOrKey: 'secret',
-}; // env להשתמש ב  ;
-module.exports = keys;
--- server/routes/newsLetterRouter.js
+++
@@ -1,8 +0,0 @@
-const NewsLetterRouter = require('express').Router();
-const NewsLetterController = require('../controllers/newsLetterController');
-
-NewsLetterRouter.post('/createMember', NewsLetterController.createMember);
-NewsLetterRouter.get('/', NewsLetterController.getAllMembers);
-NewsLetterRouter.get('/getMember/:email', NewsLetterController.getMember);
-
-module.exports = NewsLetterRouter;
