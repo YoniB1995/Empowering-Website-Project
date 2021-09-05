52927b63cbe1ba9345625bd65d93f9705ffb5918
eliasadmaso1 <76824469+eliasadmaso1@users.noreply.github.com>
2021-09-05 17:25:59 +0300

    update-product-store

--- client/src/componnets/pages/Product/Product.js
+++ client/src/componnets/pages/Product/Product.js
@@ -1,53 +1,45 @@
-import React from 'react'
+import React from "react";
 import { useParams } from "react-router-dom";
-import {useState,useEffect} from 'react';
-import {getAllProducts} from '../../../service/store-service';
-import './Product.css';
+import { useState, useEffect } from "react";
+import { getAllProducts } from "../../../service/store-service";
+import "./Product.css";
 
+const Product = () => {
+  const { id } = useParams();
+  const [product, setProduct] = useState();
+  const [like, setLike] = useState(0);
 
+  useEffect(() => {
+    getAllProducts()
+      .then((res) => res.json())
+      .then((result) => {
+        const newProduct = result.products.find((product) => {
+          return product._id === id;
+        });
+        setProduct(newProduct);
+      });
+  }, []);
 
-const Product = ()=> {
-const { id } = useParams();
+  const likesCounter = () => {
+    setLike(like + 1);
+  };
 
-const [products, setProducts] = useState([]);
-const [filteredProducts,setFilteredProducts] = useState([]);
-const [like,setLike] = useState(0);
-
-useEffect(() => {
-  getAllProducts()
-    .then((res) => res.json())
-    .then((result) => {
-    setProducts(result.products);
-    let newProductArray = products.filter((product)=>{ return product._id === id});
-    setFilteredProducts(newProductArray); 
-     })
-}, [products]);
-
-const likesCounter = ()=>{
-    setLike(like+1);
-}
-
-
-
-return(
+  return product ? (
     <div className="product-div">
-        {filteredProducts.map((product)=>{
-            return(
-            <>
-            <h2>product id : {product._id}</h2>
-            <h2>product title : {product.title}</h2>
-            <h2>product description : {product.descripation}</h2>
-            <h2>product price : {product.price}</h2>
-            <h2>product quantity : {product.quantity}</h2>
-            <h2>product variants : {product.variants}</h2>
-            <button onClick={likesCounter}>אהבתי</button>
-            likes:{like}
+      <>
+        <h2>product id : {product._id}</h2>
+        <h2>product title : {product.title}</h2>
+        <h2>product description : {product.descripation}</h2>
+        <h2>product price : {product.price}</h2>
+        <h2>product quantity : {product.quantity}</h2>
+        <h2>product variants : {product.variants}</h2>
+        <button onClick={likesCounter}>אהבתי</button>
+        likes:{like}
+      </>
+    </div>
+  ) : (
+    <div>מוצר לא נמצא</div>
+  );
+};
 
-            </>)
-        })}
-        </div>
-)
-
-}
-
-export default Product
+export default Product;
