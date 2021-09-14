a61508cf6326f207e831d03687ad809ea5b97f9e
YehudaBayana <bayanayehuda@gmail.com>
2021-09-14 12:09:38 +0300

    make plans dynamic

--- client/src/componnets/features/NavBar/NavBar.js
+++ client/src/componnets/features/NavBar/NavBar.js
@@ -1,36 +1,38 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import { Menu, Avatar } from 'antd';
-import 'antd/dist/antd.css'
+import 'antd/dist/antd.css';
 import './navBar.css';
 
- const NavBar = () => {
-    return (
-        <div className="navBar">
-            <Link to='./'><Avatar className="logo" src="logo.jpg" alt="logo"></Avatar></Link>
-            <Menu className="navBarLinks" mode="horizontal">
-                <Link to='/Donations' className="donationsBtn"> 
-                <Menu.Item >
-                לקחת חלק בשינוי
-                </Menu.Item>
-                </Link>
-                <Link to='/ContactUs'> <Menu.Item  >
-                 צור קשר
-                </Menu.Item> </Link>
-                <Link to='/Newsletter'><Menu.Item  >
-                ניוזלטר
-                </Menu.Item></Link>
-                <Link to='/Store'><Menu.Item  >
-                חנות
-                </Menu.Item></Link>
-                <Link to='/Programs'><Menu.Item  >
-                תוכניות
-                </Menu.Item></Link>
-                <Link to='/AboutUs'> <Menu.Item  >
-                עלינו
-                </Menu.Item></Link>
-            </Menu>
-        </div>
-    )
-}
+const NavBar = () => {
+  return (
+    <div className='navBar'>
+      <Link to='./'>
+        <Avatar className='logo' src='logo.jpg' alt='logo'></Avatar>
+      </Link>
+      <Menu className='navBarLinks' mode='horizontal'>
+        <Link to='/Donations' className='donationsBtn'>
+          <Menu.Item>לקחת חלק בשינוי</Menu.Item>
+        </Link>
+        <Link to='/ContactUs'>
+          {' '}
+          <Menu.Item>צור קשר</Menu.Item>{' '}
+        </Link>
+        <Link to='/Newsletter'>
+          <Menu.Item>ניוזלטר</Menu.Item>
+        </Link>
+        <Link to='/Store'>
+          <Menu.Item>חנות</Menu.Item>
+        </Link>
+        <Link to='/Plans'>
+          <Menu.Item>תוכניות</Menu.Item>
+        </Link>
+        <Link to='/AboutUs'>
+          {' '}
+          <Menu.Item>עלינו</Menu.Item>
+        </Link>
+      </Menu>
+    </div>
+  );
+};
 
 export default NavBar;
