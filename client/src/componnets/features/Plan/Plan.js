41de943738da1037f03534d4c6f60a83379f11e4
YehudaBayana <76784836+YehudaBayana@users.noreply.github.com>
2021-09-14 12:10:33 +0300

    make plans dynamic (#100)

--- client/src/componnets/features/Plan/Plan.js
+++ client/src/componnets/features/Plan/Plan.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { getAllPlans } from '../../../service/plan-service';
 import './Plan.css';
 
@@ -21,47 +21,18 @@ const Plan = () => {
         <div class='container'>
           <div class='page-container'>
             <article class='article'>
-              <img
-                src='https://source.unsplash.com/crVO0UMdoVU/600x600'
-                alt=''
-              />
-              <h1 class='l-heading'>{plan?.title}</h1>
+              <h1 class='l-heading'>title: {plan?.title}</h1>
               <div class='meta'>
                 <small>
-                  <i class='fas fa-user'></i> Written By Yuda
+                  <i class='fas fa-user'></i>sanitizedHtml:
+                  {plan?.sanitizedHtml}
                 </small>
-                <div class='category category-ent'>Entertainment</div>
+                <div class='category category-ent'>slug: {plan?.slug}</div>
               </div>
-              <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
-                debitis beatae ad doloremque voluptate, blanditiis suscipit
-                error quam fugit sapiente, aliquid quia libero incidunt
-                recusandae enim voluptas nulla sequi similique? Temporibus ad
-                est minima magni molestias obcaecati consequatur cumque
-                exercitationem deleniti eos, animi facere recusandae, pariatur
-                eveniet neque? Voluptatem, molestias.
-              </p>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae
-                dignissimos dolorum ipsam corporis similique! Quis, esse minus?
-                Ducimus tempore provident sunt officia? Velit temporibus harum
-                voluptate, ipsum quisquam rerum doloremque consectetur quos
-                incidunt deleniti dicta excepturi ratione, fugiat ad fuga in nam
-                dolorum aut minus esse voluptatum suscipit et quae. Repellendus
-                minus, ratione aperiam commodi voluptas iusto aliquam sapiente.
-                Aut obcaecati, corporis praesentium porro nostrum excepturi
-                animi repellendus incidunt non quis nisi assumenda voluptas
-                possimus molestiae laboriosam iure sed architecto in perferendis
-                magnam blanditiis voluptatum placeat labore odio? Unde id dolore
-                aliquam, quas sit harum, saepe maiores facilis aliquid molestiae
-                qui. Praesentium rerum, minima fugit laudantium quaerat
-                voluptatem pariatur sequi saepe blanditiis nobis natus possimus
-                quo, assumenda numquam sit, eaque accusamus incidunt
-                exercitationem quis deleniti quisquam corrupti vel? Ut quisquam
-                consequatur, doloribus voluptatibus iusto quaerat, sint non
-                magnam reiciendis amet quos sapiente excepturi omnis laboriosam
-                nobis, expedita delectus odio provident?
-              </p>
+              <p>description: {plan?.description}</p>
+              <p>markdown: {plan?.markdown}</p>
+              <h3>{plan?.createdAt}</h3>
+              <Link to='/Plans'>back to plans</Link>
             </article>
 
             <aside id='categories' class='article'>
