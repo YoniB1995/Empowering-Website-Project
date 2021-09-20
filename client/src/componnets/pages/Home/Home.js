e1a60b72a129edc047762149857983cdbb53ae5e
YehudaBayana <bayanayehuda@gmail.com>
2021-09-20 16:54:53 +0300

    hard coded campaigns for newsletter

--- client/src/componnets/pages/Home/Home.js
+++ client/src/componnets/pages/Home/Home.js
@@ -1,17 +1,18 @@
-import React from "react";
-import "./Home.css";
-import GoalsCards from "./Goals/GoalsCards";
-import WorkersCards from "../../features/workersCards/WorkersCards";
-import ImageSlider from "../../features/Slider/SliderStore"
-import "./Home.css";
-import "animate.css";
-import Vision from "./vision/Vision";
-import "animate.css";
-import SliderStore from "../../features/Slider/SliderStore";
-import { useEffect } from "react";
-import WOW from "wowjs";
+import React from 'react';
+import './Home.css';
+import GoalsCards from './Goals/GoalsCards';
+import WorkersCards from '../../features/workersCards/WorkersCards';
+import ImageSlider from '../../features/Slider/SliderStore';
+import './Home.css';
+import 'animate.css';
+import Vision from './vision/Vision';
+import 'animate.css';
+import SliderStore from '../../features/Slider/SliderStore';
+import { useEffect } from 'react';
+import WOW from 'wowjs';
 import { BackTop } from 'antd';
-import Store from "../Store/Store";
+import Store from '../Store/Store';
+import Newsletter from '../Newsletter/Newsletter';
 const Home = () => {
   useEffect(() => {
     new WOW.WOW({
@@ -20,9 +21,9 @@ const Home = () => {
   }, []);
   return (
     <div>
-      <div className="home-first-section">
-        <ImageSlider/>
-        <div className="about-us wow animate__bounceInUp">
+      <div className='home-first-section'>
+        <ImageSlider />
+        <div className='about-us wow animate__bounceInUp'>
           <h1>קצת עלינו </h1>
           <p>
             <br /> העצמה נשית אתיופית, עידוד ייעוץ ומילגות להשכלה גבוהה לנשים,
@@ -30,7 +31,7 @@ const Home = () => {
             <br />
           </p>
           <p>
-            {" "}
+            {' '}
             ייעוץ לעצמאות כלכלית, טיפול בפניות הציבור נזקקים, נפגעי תקיפה מינית,
             <br /> נשים הסובלות מאלימות
           </p>
@@ -51,25 +52,26 @@ const Home = () => {
           </p>
         </div>
       </div>
-      <div className="buffer-div">
-        <div className="arrow-icon-container">
+      <div className='buffer-div'>
+        <div className='arrow-icon-container'>
           <i
-            class="fas fa-arrow-down"
-            style={{ fontSize: "50px", color: "white" }}
+            class='fas fa-arrow-down'
+            style={{ fontSize: '50px', color: 'white' }}
           ></i>
         </div>
       </div>
-      <div className="home-second-section">
+      <div className='home-second-section'>
         <WorkersCards />
       </div>
       <Vision />
       <GoalsCards />
       {/* <Copartner/> */}
       <SliderStore />
+      <Newsletter />
       {/* <Store/> */}
       <BackTop>
-      <div className="upBtn">BACK TO UP</div>
-    </BackTop>
+        <div className='upBtn'>BACK TO UP</div>
+      </BackTop>
     </div>
   );
 };
