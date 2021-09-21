40de6161250df5247c11ab297f844812303f8880
Lior3796 <liorlsa9@gmail.com>
2021-09-21 23:37:17 +0300

    comment regex model validation

--- server/models/adminModel.js
+++ server/models/adminModel.js
@@ -1,49 +1,43 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Joi = require("joi");
-const { required } = require('joi');
-const Joigoose = require('joigoose');
+const { required } = require("joi");
+const Joigoose = require("joigoose");
 
 const joigoose = require("joigoose")(mongoose);
 
+const adminSchema = Joi.object(
+	{
+		username: Joi.string().min(4).max(199).required(),
+	},
+	{
+		password: Joi.string()
+			// .regex('/^[a-2A-Z0-9],{3,30}/')
+			.min(6)
+			.max(15)
+			.required(),
+		// password_confirmation: Joi.any().valid(Joi.ref('password')).required().options({ language: { any: { allowOnly: 'must match password' } } })
+	},
+	{
+		email: Joi.string()
+			.email()
+			.min(5)
+			.max(99)
+			// .regex('^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
+			.required(),
+	}
+);
 
-const adminSchema = Joi.object({
-  username: Joi.string()
-  .min(4)
-  .max(199)
-  .required()
-  
-},
-{
-  password: Joi.string()
-  .regex('/^[a-2A-Z0-9],{3,30}/')
-  .min(6)
-  .max(15)
-  .required()
-  // password_confirmation: Joi.any().valid(Joi.ref('password')).required().options({ language: { any: { allowOnly: 'must match password' } } })
-  
-},
-{
-  email: Joi.string()
-  .email()
-  .min(5)
-  .max(99)
-  .regex('^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
-  .required()
-  
-})
+const validAdmin = (admin) => {
+	const adminJoi = adminSchema.validate(admin);
+	return adminJoi;
+};
 
+const adminMongoose = new mongoose.Schema(joigoose.convert(adminSchema));
 
-const validAdmin = (admin)=>{
-  const adminJoi = adminSchema.validate(admin)
-  return adminJoi
-}
+const adminModel = mongoose.model("admin", adminMongoose);
 
-const adminMongoose = new mongoose.Schema(joigoose.convert(adminMongoose))
-
-const adminModel = mongoose.model('admin',adminMongoose)
-
-module.exports={
-  validAdmin,
-  adminModel
-}
+module.exports = {
+	validAdmin,
+	adminModel,
+};
--- server/models/contactUsModel.js
+++ server/models/contactUsModel.js
@@ -1,51 +1,43 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Joi = require("joi");
-const { boolean } = require('joi');
+const { boolean } = require("joi");
 
 const joigoose = require("joigoose")(mongoose);
 
+const contactUsSchema = Joi.object(
+	{
+		email: Joi.string()
+			.email()
+			.min(5)
+			.max(99)
+			// .regex('^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
+			.required(),
+	},
+	{
+		inquiry: Joi.string().min(5).max(99).required(),
+	},
+	{
+		content: Joi.string().min(5).max(99).required(),
+	},
+	{
+		iscompleted: Joi.boolean().required(),
+	},
+	{
+		notes: Joi.string().required(),
+	}
+);
 
-const contactUsSchema = Joi.object({
-  email: Joi.string()
-  .email()
-  .min(5)
-  .max(99)
-  .regex('^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
-  .required()
+const validContact = (contact) => {
+	const contactJoi = contactSchema.validate(contact);
+	return contactJoi;
+};
 
-},
-{
-  inquiry: Joi.string()
-  .min(5)
-  .max(99)
-  .required()
-},
-{
-  content: Joi.string()
-  .min(5)
-  .max(99)
-  .required()
-},
-{
-  iscompleted:Joi.boolean()
-  .required()
-},
-{
-  notes:Joi.string()
-  .required()
-})
+const contactMongoose = new mongoose.Schema(joigoose.convert(contactUsSchema));
 
-const validContact = (contact)=>{
-  const contactJoi = contactSchema.validate(contact)
-  return contactJoi
-}
+const contactModel = mongoose.model("contact", contactMongoose);
 
-const contactMongoose = new mongoose.Schema(joigoose.convert(contactMongoose))
-
-const contactModel = mongoose.model('contact',contactMongoose)
-
-module.exports={
-  validContact,
-  contactModel
-}
+module.exports = {
+	validContact,
+	contactModel,
+};
