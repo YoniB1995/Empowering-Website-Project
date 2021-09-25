df1ab561edc165f503d319a41f9541959e22015b
HanaAmara <hhanaamara@gmail.com>
2021-09-25 17:59:27 +0300

    models fixed

--- server/models/adminModel.js
+++ server/models/adminModel.js
@@ -1,43 +1,40 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const Joi = require("joi");
-const { required } = require("joi");
-const Joigoose = require("joigoose");
 
 const joigoose = require("joigoose")(mongoose);
+const adminSchema = Joi.object({
+  username: Joi.string()
+  .min(4)
+  .max(199)
+  .required(),
+  password: Joi.string()
+  .min(6)
+  .max(15)
+  .required()
+  // password_confirmation: Joi.any().valid(Joi.ref('password')).required().options({ language: { any: { allowOnly: 'must match password' } } })
+ ,
+//  return res.json( validBody.error.details[0].message).status(400)
 
-const adminSchema = Joi.object(
-	{
-		username: Joi.string().min(4).max(199).required(),
-	},
-	{
-		password: Joi.string()
-			// .regex('/^[a-2A-Z0-9],{3,30}/')
-			.min(6)
-			.max(15)
-			.required(),
-		// password_confirmation: Joi.any().valid(Joi.ref('password')).required().options({ language: { any: { allowOnly: 'must match password' } } })
-	},
-	{
-		email: Joi.string()
-			.email()
-			.min(5)
-			.max(99)
-			// .regex('^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
-			.required(),
-	}
-);
+  email: Joi.string()
+  .email()
+  .min(5)
+  .max(99)
+  .required()
+  
+})
 
-const validAdmin = (admin) => {
-	const adminJoi = adminSchema.validate(admin);
-	return adminJoi;
-};
 
-const adminMongoose = new mongoose.Schema(joigoose.convert(adminSchema));
+const validAdmin = (admin)=>{
+  const adminJoi = adminSchema.validate(admin)
+  return adminJoi
+}
 
-const adminModel = mongoose.model("admin", adminMongoose);
+const adminMongoose = new mongoose.Schema(joigoose.convert(adminSchema))
 
-module.exports = {
-	validAdmin,
-	adminModel,
-};
+const adminModel = mongoose.model('admin',adminMongoose)
+
+module.exports={
+  validAdmin,
+  adminModel
+}
--- server/models/teamModel.js
+++ server/models/teamModel.js
@@ -1,13 +1,39 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const { Schema } = mongoose;
+// const { Schema } = mongoose;
 
-const teamSchema = new Schema({ fullname: String,
-  role: String,
-  image: String,
-  description:String });
+// const teamSchema = new Schema({ fullname: String,
+//   role: String,
+//   image: String,
+//   description:String });
 
 
 
-const team = mongoose.model('team', teamSchema)
-module.exports = team;
+// const team = mongoose.model('team', teamSchema)
+// module.exports = team;
+
+const mongoose  = require('mongoose');
+
+const Joi = require('joi')
+
+const Joigoose = require('joigoose')(mongoose)
+
+const teamSchema = Joi.object({
+  role: Joi.string() ,
+  image: Joi.string(),
+  description: Joi.string()
+})
+
+const validTeam = (team) =>{
+  const teamJoi =  teamSchema.validate(team)
+  return teamJoi;
+}
+
+const teamMongooseSchema = mongoose.Schema(Joigoose.convert(teamSchema))
+
+const teamModel = mongoose.model('team',teamMongooseSchema)
+
+module.exports = {
+  validTeam,
+  teamModel
+}
