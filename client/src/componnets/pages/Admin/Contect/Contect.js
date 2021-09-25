a406326287250710b8d4bda36f08c3ef2fd6bac2
michal212 <michalfarada1997@gmail.com>
2021-09-25 18:29:30 +0300

    updated branch

--- client/src/componnets/pages/Admin/Contect/Contect.js
+++ client/src/componnets/pages/Admin/Contect/Contect.js
@@ -1,15 +1,30 @@
+<<<<<<< HEAD
 import { Table } from "antd";
 import PartnersContect from "./Partners-Contect";
 import ProgramsContect from "./Programs-Contect";
 import StaffContect from "./Staff-Contect";
 // import AdminNavBar from "../../Features/Admin-Nav/Admin-Navbar";
+=======
+import { Table } from 'antd';
+import PartnersContect from './Partners-Contect';
+import ProgramsContect from './Programs-Contect';
+// import StaffContect from './Staff-Contect';
+import EditableTable from './EditableTable/EditableTable';
+import AdminNavBar from '../../../features/Admin/Admin-Nav/Admin-Navbar';
+>>>>>>> d88b2420d03e0bfb43d9a8a1254b5ed63284a767
 
 const { Column, ColumnGroup } = Table;
 
 const Contect = () => {
   return (
     <>
+<<<<<<< HEAD
       <StaffContect />
+=======
+      <AdminNavBar />
+      {/* <StaffContect /> */}
+      <EditableTable />
+>>>>>>> d88b2420d03e0bfb43d9a8a1254b5ed63284a767
       <ProgramsContect />
       <PartnersContect />
     </>
