51ae0166b2416583d9772a0b0daa66f64162bb6b
michal212 <michalfarada1997@gmail.com>
2021-09-25 18:22:37 +0300

    updated branch

--- client/src/componnets/pages/Admin/Contect/Contect.js
+++ client/src/componnets/pages/Admin/Contect/Contect.js
@@ -7,63 +7,12 @@ import AdminNavBar from '../../../features/Admin/Admin-Nav/Admin-Navbar';
 
 const { Column, ColumnGroup } = Table;
 
-const data = [
-  {
-    key: '1',
-    firstName: 'John',
-    lastName: 'Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
-  },
-  {
-    key: '2',
-    firstName: 'Jim',
-    lastName: 'Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    tags: ['loser'],
-  },
-  {
-    key: '3',
-    firstName: 'Joe',
-    lastName: 'Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
-  },
-  {
-    key: '1',
-    firstName: 'John',
-    lastName: 'Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
-  },
-  {
-    key: '2',
-    firstName: 'Jim',
-    lastName: 'Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    tags: ['loser'],
-  },
-  {
-    key: '3',
-    firstName: 'Joe',
-    lastName: 'Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
-  },
-];
-
 const Contect = () => {
   return (
     <>
       <AdminNavBar />
       {/* <StaffContect /> */}
-      <EditableTable />
+      {/* <EditableTable /> */}
       <ProgramsContect />
       <PartnersContect />
     </>
