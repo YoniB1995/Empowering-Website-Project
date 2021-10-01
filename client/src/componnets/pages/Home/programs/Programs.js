c3ea7322bd5742975e3c4f1885e2fbf10b30db52
YehudaBayana <bayanayehuda@gmail.com>
2021-10-02 00:21:57 +0300

    programs getting data from db

--- client/src/componnets/pages/Home/programs/Programs.js
+++ client/src/componnets/pages/Home/programs/Programs.js
@@ -1,38 +1,25 @@
-import "animate.css";
-import { Button, Card } from "antd";
-import { useEffect, useState } from "react";
-import WOW from "wowjs";
-import { ReadOutlined  } from "@ant-design/icons";
+import 'animate.css';
+import { Button, Card } from 'antd';
+import { useEffect, useState } from 'react';
+import WOW from 'wowjs';
+import { ReadOutlined } from '@ant-design/icons';
 
-import "./programs.css";
+import './programs.css';
 const Programs = () => {
   const { Meta } = Card;
   const [clickedIndex, setClickedIndex] = useState(false);
+  const [programs, setPrograms] = useState([]);
   const cardProgramOn = 'color:"white",marginLeft:"5px"';
 
   useEffect(() => {
     new WOW.WOW({
       live: false,
     }).init();
-  }, []);
 
-  const programs = [
-    {
-      title: "קורס יזמות עסקית",
-      description:
-        "התוכניות מיועדת לבעלות עזקים ויזמיות בתחומים מגוונים בעלות פוטנציאל אישי עסקי ואישי התוכנית תקנה כלים ניהוליים בתהליך המורכב של הפיכת רעיון לעסק מצליח בתוכנית ילמדו הנשים איך לבנות תוכנית עבודה ואיך להגדיר צעדים להקמת עסק התוכנית היא בשיתוף פעולה ",
-    },
-    {
-      title: "השכלה גבוהה",
-      description:
-        " תוכנית השכלה גבוהה תיתן הכוונה וייעוץ לנשים אשר מעוניינות ללמוד ולשפר את איכות חייהן וחיי משפחתן התוכנית תחשוף בפני הנשים את המוסדות הלימוד ותוכניות הקיימות בכל הארץ ואף תסייע במציאת מלגות ונשאף אף למתן מלגות לימוד",
-    },
-    {
-      title: "תוכנית העצמה נשית",
-      description:
-        "הכרת כלים לשיפור היכולת האישית של נשים בעבודה בחברה ובמשפלה ובינהם חוק למניעת הטרדה מינית חוק עבודת נשים וחוק שווי זכויות האישה חוק שיווי זכויות האישה וחוק השיוון זכיות הזדמניות  אסרטביות ככלי מרכזי להעצמת נשים הפמינזם ככלי מרכזי להעצמת נשים",
-    },
-  ];
+    fetch('http://localhost:5000/plans/all/hebrew')
+      .then((res) => res.json())
+      .then((data) => setPrograms(data));
+  }, []);
 
   const ImgDisappearedOnHover = (index) => () => {
     setClickedIndex((state) => ({
@@ -42,38 +29,39 @@ const Programs = () => {
   };
 
   return (
-    <div className="programs-wrapper">
-      <div className="programs-header">
-        <h3 style={{ color: "#D57E7E", fontSize: "32px" }}>תוכניות</h3>
-        <div className="programs-header-decertion"></div>
+    <div className='programs-wrapper'>
+      <div className='programs-header'>
+        <h3 style={{ color: '#D57E7E', fontSize: '32px' }}>תוכניות</h3>
+        <div className='programs-header-decertion'></div>
       </div>
-      <div className="cards-wrapper">
+      <div className='cards-wrapper'>
         {programs.map((program, index) => (
           <Card
-            className="program-card"
+            className='program-card'
             onClick={ImgDisappearedOnHover(index)}
             hoverable
             cover={
               clickedIndex[index] ? (
-                <div className="program-card animate__animated animate__fadeInUp">
-                  <div className="card-program-clicked-container">
-                    <h2 className="program-clicked-header">{program.title} </h2>
-                    <p className="program-clicked-description">
+                <div className='program-card animate__animated animate__fadeInUp'>
+                  <div className='card-program-clicked-container'>
+                    <h2 className='program-clicked-header'>{program.title} </h2>
+                    <p className='program-clicked-description'>
                       {program.description}
                     </p>
                     <div>
                       <img
-                        className="img-program-card"
-                        src="./logo-main1.jpeg" alt="program card img"
+                        className='img-program-card'
+                        src='./logo-main1.jpeg'
+                        alt='program card img'
                       />
                     </div>
                   </div>
                 </div>
               ) : (
                 <img
-                  className="program-card animate__animated animate__fadeIn"
-                  alt="example"
-                  src="./woman-vector.jpg"
+                  className='program-card animate__animated animate__fadeIn'
+                  alt='example'
+                  src='./woman-vector.jpg'
                 />
               )
             }
@@ -81,9 +69,12 @@ const Programs = () => {
             <Meta
               title={
                 clickedIndex[index] ? (
-                  ""
+                  ''
                 ) : (
-                  <h3 className="program-card-title">{program.title}<ReadOutlined style={{marginLeft:"4px"}} /></h3>
+                  <h3 className='program-card-title'>
+                    {program.title}
+                    <ReadOutlined style={{ marginLeft: '4px' }} />
+                  </h3>
                 )
               }
             />
