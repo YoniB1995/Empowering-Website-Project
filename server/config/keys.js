4024c87b71213096a568703431c6e455605857b9
michal212 <michalfarada1997@gmail.com>
2021-10-01 22:26:00 +0300

    real contect final

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,7 +1,7 @@
 /* eslint-disable camelcase */
 require('dotenv').config()
 const keys = {
-  mongoURI:"mongodb+srv://empowering2021:EMpower1234@cluster0.mwn5e.mongodb.net/myFirstDatabase?retryWrites=true&w=majorityL", 
-  secretOrKey:"yoniB1995"
+  mongoURI:process.env.CONNECTION_URL,
+  secretOrKey:process.env.SECRET_KEY,
 }
 module.exports = keys;
--- server/db/index.js
+++ server/db/index.js
@@ -2,20 +2,20 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-	await mongoose.connect("mongodb+srv://empowering2021:EMpower1234@cluster0.mwn5e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useCreateIndex: true,
-		useFindAndModify: false,
-	});
+  await mongoose.connect(process.env.CONNECTION_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  });
 };
 connectDB()
-	.then(() => {
-		console.log("MongoDB Atlas Connected");
-		// importData();
-	})
-	.catch((err) => {
-		console.log(`Error Message - ${err.message}`);
-	});
+  .then(() => {
+    console.log("MongoDB Atlas Connected");
+    // importData();
+  })
+  .catch((err) => {
+    console.log(`Error Message - ${err.message}`);
+  });
 
 module.exports = mongoose.connection;
