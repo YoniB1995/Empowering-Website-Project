3cbd4c4cc1d1b6626aa6b5133bde40cb7404b940
Lior3796 <liorlsa9@gmail.com>
2021-10-01 11:50:44 +0300

    add delete test

--- server/utils/tests-classes/teamClass.js
+++ server/utils/tests-classes/teamClass.js
@@ -1,18 +1,24 @@
 const { teamModel } = require("../../models/teamModel");
 class teamMembership {
-  constructor() {
-    this.user = teamModel;
-  }
-  async create(fullname, description) {
-    return this.user.create({
-      fullname,
-      
-      description
-    });
-  }
-  async getUser(id) {
-    return this.user.findOne({ id });
-  }
+	constructor() {
+		this.user = teamModel;
+	}
+	async create(fullname, description) {
+		return this.user.create({
+			fullname,
+
+			description,
+		});
+	}
+	async getUser(id) {
+		return this.user.findOne({ id });
+	}
+
+	async deleteUser(_id) {
+		const result = await this.user.findByIdAndDelete({ _id });
+		console.log(result);
+		return result;
+	}
 }
 
 module.exports = teamMembership;
