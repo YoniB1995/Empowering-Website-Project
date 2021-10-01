848df3ec80577a6a16918dc5bcb7147b77536ba4
YoniB1995 <yonatansamfisher@gmail.com>
2021-10-01 18:12:32 +0300

    updated partners frontend and partners-service

---
+++ client/src/service/partners-service.js
@@ -0,0 +1,13 @@
+const API = process.env.NODE_ENV === "production"
+ ? `https://empowering-women-web.herokuapp.com/`: "http://localhost:5000";
+
+
+export const getAllPartners = async (req, res) => {
+  try {
+    return await fetch(`${API}/partners/all`);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
