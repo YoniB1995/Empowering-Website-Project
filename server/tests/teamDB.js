c2ae225b49570187d718ac75022888423843cad5
YoniB1995 <yonatansamfisher@gmail.com>
2021-10-02 18:13:03 +0300

    updated tests

---
+++ server/tests/teamDB.js
@@ -0,0 +1,347 @@
+ const teamDB = {
+    "getAllTeamMembers": {
+  "success": {
+    "res": {
+      "statusCode": 200,
+      "headers": {
+        "content-type": "application/json"
+      }
+    },
+    "body": {
+      "status": "success",
+      "data": [
+        {
+            "_id": "6133bca1faebad48f88b8323",
+            "fullname": "Shmuel Ezra",
+            "role": "Board of Directors",
+            "image": "https://serving.photos.photobox.com/6048836667ccbc894a631d7d20a9dfb97afbab988a4fc1ae7bfc4b8b75586d7af999497d.jpg",
+            "description": "Battalion Commander, Bachelor's degree in Government / Diplomacy / Strategy and a graduate of the \"Nachshon\" Preparatory School.",
+            "__v": 0,
+            "lang": "english"
+        },
+        {
+            "_id": "6133bca1faebad48f88b8320",
+            "fullname": "Racheli Tadesa Melkai",
+            "role": "Founder",
+            "image": "https://medias.atmag.co.il/www/uploads/2019/02/IMG_7484color-2000x1125.jpg",
+            "description": "Racheli Tadasa Malkai - Entrepreneur, founder and CEO of the association, was born in Ethiopia in 1983,\n Immigrated to Israel with her family in 1991 \"In Operation Solomon\" married +3, with a B.A. in social work at Ashkelon College.",
+            "__v": 0,
+            "lang": "english"
+        },
+        {
+            "_id": "6133bca1faebad48f88b8321",
+            "fullname": "Macy Sahalo",
+            "role": "Board of Directors",
+            "image": "https://serving.photos.photobox.com/150067982369ca33979867f5f1d4d5c09c58239733f30dcd456058c32d8e23eada450959.jpg",
+            "description": "Acting Chairman of the Board of Directors",
+            "__v": 0,
+            "lang": "hebrew"
+        },
+        {
+            "_id": "6133bca1faebad48f88b8324",
+            "fullname": "Sivan Tyme",
+            "role": "Board of Directors",
+            "image": "https://serving.photos.photobox.com/12272849bdd02df3026299cdef1781004ed804fed03777e37300b6ebf7f4745d5a0897a4.jpg",
+            "description": "Partner Communications Company",
+            "__v": 0,
+            "lang": "english"
+        },
+        {
+            "_id": "6133bca1faebad48f88b8322",
+            "fullname": "Dani Limor",
+            "role": "Board of Directors",
+            "image": "https://serving.photos.photobox.com/98855025c4cde414137dd88b9baa37bf54da601b7edd4ab3adcde2de455c0b7ca4046551.jpg",
+            "description": " Retiree of the Ministry of Defense (rights-intensive), one of the founders of the Nachshon Preparatory School.",
+            "__v": 0,
+            "lang": "english"
+        },
+        {
+            "_id": "614eee25657f2814c3e767d4",
+            "fullname": "Sheli Avraham",
+            "role": "Board of Directors",
+            "image": "https://www.thezamarrinstitute.org/wp-content/uploads/2016/07/no-profile-img-221x300.gif",
+            "description": "Designer of clothing and art education",
+            "__v": 0,
+            "lang": "english"
+        },
+        {
+            "_id": "614eee4a657f2814c3e767d5",
+            "fullname": "Batya Shmueli",
+            "role": "Board of Directors",
+            "image": "https://serving.photos.photobox.com/13716631e3479af7b5c82436a70408fdef0b10dec9098bd3745f7f8878a977a691ac21a5.jpg",
+            "description": "Group facilitator and emissary in South Africa on behalf of the Jewish Agency",
+            "__v": 0,
+            "lang": "english"
+        },
+        {
+            "_id": "614eee6b657f2814c3e767d6",
+            "fullname": "Ruti Geneva Amara",
+            "role": "Board of Directors",
+            "image": "https://serving.photos.photobox.com/19048505d80424d2401cd5a0f13c840192326ca83defa389108db6fc7cd9f23846edf06d.jpg",
+            "description": "Independent",
+            "__v": 0,
+            "lang": "english"
+        },
+        {
+            "_id": "6151ae072b79f1be18e6111b",
+            "fullname": "Yoni Bitew",
+            "role": "Volunteers",
+            "image": "https://www.thezamarrinstitute.org/wp-content/uploads/2016/07/no-profile-img-221x300.gif",
+            "description": "This is a description about a person who helps building the website and jsjgi sjg sdg jig ",
+            "__v": 0,
+            "lang": "english"
+        },
+        {
+            "_id": "6151ae402b79f1be18e6111c",
+            "fullname": "Lior Solomon",
+            "role": "Volunteers",
+            "image": "https://www.thezamarrinstitute.org/wp-content/uploads/2016/07/no-profile-img-221x300.gif",
+            "description": "This is a description about a person who helps building the website and jsjgi sjg sdg jig ",
+            "__v": 0,
+            "lang": "english"
+        },
+        {
+            "_id": "6151ae532b79f1be18e6111d",
+            "fullname": "Asi Alena",
+            "role": "Volunteers",
+            "image": "https://www.thezamarrinstitute.org/wp-content/uploads/2016/07/no-profile-img-221x300.gif",
+            "description": "This is a description about a person who helps building the website and jsjgi sjg sdg jig ",
+            "__v": 0,
+            "lang": "english"
+        },
+        {
+            "_id": "6151ae622b79f1be18e6111e",
+            "fullname": "Hana Amara",
+            "role": "Volunteers",
+            "image": "https://www.thezamarrinstitute.org/wp-content/uploads/2016/07/no-profile-img-221x300.gif",
+            "description": "This is a description about a person who helps building the website and jsjgi sjg sdg jig ",
+            "__v": 0,
+            "lang": "english"
+        },
+        {
+            "_id": "6151ae6d2b79f1be18e6111f",
+            "fullname": "Beni Yaakov",
+            "role": "Volunteers",
+            "image": "https://www.thezamarrinstitute.org/wp-content/uploads/2016/07/no-profile-img-221x300.gif",
+            "description": "NEW This is a description about a person who helps building the website and jsjgi sjg sdg jig ",
+            "__v": 0,
+            "lang": "english"
+        }     
+      ]
+    }
+  },
+  "failure": {
+    "res": {
+      "statusCode": 404,
+      "headers": {
+        "content-type": "application/json"
+      }
+    },
+    "body": {
+      "status": "error",
+      "message": "That movie does not exist."
+    }
+  }
+},
+ "getOneTeamMember": {
+  "success": {
+    "res": {
+      "statusCode": 200,
+      "headers": {
+        "content-type": "application/json"
+      }
+    },
+    "body": {
+      "status": "success",
+      "data": [
+         {
+            "_id": "614eee25657f2814c3e767d4",
+            "fullname": "Sheli Avraham",
+            "role": "Board of Directors",
+            "image": "https://www.thezamarrinstitute.org/wp-content/uploads/2016/07/no-profile-img-221x300.gif",
+            "description": "Designer of clothing and art education",
+            "__v": 0,
+            "lang": "english"
+        }
+      ]
+    }
+  },
+  "failure": {
+    "res": {
+      "statusCode": 404,
+      "headers": {
+        "content-type": "application/json"
+      }
+    },
+    "body": {
+      "status": "error",
+      "message": "Team member details not exist."
+    }
+  }
+},
+ "getTeamMemberByRole": {
+  "success": {
+    "res": {
+      "statusCode": 200,
+      "headers": {
+        "content-type": "application/json"
+      }
+    },
+    "body": {
+      "status": "success",
+      "message":"List of Board of Directors",
+      "data": [
+          {
+            "_id": "6133bca1faebad48f88b8323",
+            "fullname": "Shmuel Ezra",
+            "role": "Board of Directors",
+            "image": "https://serving.photos.photobox.com/6048836667ccbc894a631d7d20a9dfb97afbab988a4fc1ae7bfc4b8b75586d7af999497d.jpg",
+            "description": "Battalion Commander, Bachelor's degree in Government / Diplomacy / Strategy and a graduate of the \"Nachshon\" Preparatory School.",
+            "__v": 0,
+            "lang": "english"
+        },
+        {
+            "_id": "6133bca1faebad48f88b8321",
+            "fullname": "Macy Sahalo",
+            "role": "Board of Directors",
+            "image": "https://serving.photos.photobox.com/150067982369ca33979867f5f1d4d5c09c58239733f30dcd456058c32d8e23eada450959.jpg",
+            "description": "Acting Chairman of the Board of Directors",
+            "__v": 0,
+            "lang": "english"
+        },
+        {
+            "_id": "6133bca1faebad48f88b8324",
+            "fullname": "Sivan Tyme",
+            "role": "Board of Directors",
+            "image": "https://serving.photos.photobox.com/12272849bdd02df3026299cdef1781004ed804fed03777e37300b6ebf7f4745d5a0897a4.jpg",
+            "description": "Partner Communications Company",
+            "__v": 0,
+            "lang": "english"
+        },
+        {
+            "_id": "6133bca1faebad48f88b8322",
+            "fullname": "Dani Limor",
+            "role": "Board of Directors",
+            "image": "https://serving.photos.photobox.com/98855025c4cde414137dd88b9baa37bf54da601b7edd4ab3adcde2de455c0b7ca4046551.jpg",
+            "description": " Retiree of the Ministry of Defense (rights-intensive), one of the founders of the Nachshon Preparatory School.",
+            "__v": 0,
+            "lang": "english"
+        },
+        {
+            "_id": "614eee25657f2814c3e767d4",
+            "fullname": "Sheli Avraham",
+            "role": "Board of Directors",
+            "image": "https://www.thezamarrinstitute.org/wp-content/uploads/2016/07/no-profile-img-221x300.gif",
+            "description": "Designer of clothing and art education",
+            "__v": 0,
+            "lang": "english"
+        },
+        {
+            "_id": "614eee4a657f2814c3e767d5",
+            "fullname": "Batya Shmueli",
+            "role": "Board of Directors",
+            "image": "https://serving.photos.photobox.com/13716631e3479af7b5c82436a70408fdef0b10dec9098bd3745f7f8878a977a691ac21a5.jpg",
+            "description": "Group facilitator and emissary in South Africa on behalf of the Jewish Agency",
+            "__v": 0,
+            "lang": "english"
+        },
+        {
+            "_id": "614eee6b657f2814c3e767d6",
+            "fullname": "Ruti Geneva Amara",
+            "role": "Board of Directors",
+            "image": "https://serving.photos.photobox.com/19048505d80424d2401cd5a0f13c840192326ca83defa389108db6fc7cd9f23846edf06d.jpg",
+            "description": "Independent",
+            "__v": 0,
+            "lang": "english"
+        }
+      ]
+    }
+  },
+  "failure": {
+    "res": {
+      "statusCode": 404,
+      "headers": {
+        "content-type": "application/json"
+      }
+    },
+    "body": {
+      "status": "error",
+      "message": "Team member details not exist."
+    }
+  }
+},
+    "addTeamMember": {
+  "success": {
+    "res": {
+      "statusCode": 201,
+      "headers": {
+        "content-type": "application/json"
+      }
+    },
+    "body": {
+      "status": "success",
+      "message":"New Team Member Added!",
+      "data": [
+        {
+          "_id": "614b9fa0df92314c81d69f06",
+            "fullname": "Racheli Melkai",
+            "role": "Founder",
+            "image": "https://www.iconsdb.com/icons/preview/red/administrator-xxl.png",
+            "description": "This is a test description for testing API new added",
+            "lang": "english"
+        }
+      ]
+    }
+  },
+  "failure": {
+  "res": {
+    "statusCode": 400,
+    "headers": {
+      "content-type": "application/json"
+    }
+  },
+  "body": {
+    "status": "error",
+    "message": "Something went wrong."
+  }
+}
+},
+"updateTeamDetails": {
+  "success": {
+    "res": {
+      "statusCode": 201,
+      "headers": {
+        "content-type": "application/json"
+      }
+    },
+    "body": {
+      "status": "success",
+      "message":"team details has changed!",
+      "data": [
+        {
+          "_id": "614b9fa0df92314c81d69f06",
+            "fullname": "Racheli Melkai",
+            "role": "Founder",
+            "image": "https://www.iconsdb.com/icons/preview/red/administrator-xxl.png",
+            "description": "This is a test description for testing API new added",
+            "lang": "english"
+        }
+      ]
+    }
+  },
+  "failure": {
+  "res": {
+    "statusCode": 404,
+    "headers": {
+      "content-type": "application/json"
+    }
+  },
+  "body": {
+    "status": "error",
+    "message": "Team memeber details does not exists."
+  }
+}
+}
+}
+
+module.exports = teamDB;
