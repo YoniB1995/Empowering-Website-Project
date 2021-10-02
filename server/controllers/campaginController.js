302bebd55ecedccafa6b3b35e15cebb1ea311344
Lior3796 <liorlsa9@gmail.com>
2021-10-02 12:51:17 +0300

    add classCampagin and tests

--- server/controllers/campaginController.js
+++ server/controllers/campaginController.js
@@ -27,63 +27,6 @@ const getCampaignsSorted = async (req, res, next) => {
 	}
 };
 
-const getCampaignByTitle = async (req, res, next) => {
-	try {
-		const campaignsList = await MailchimpMarketingModel.campaigns.list();
-		const { title } = req.params;
-
-		const chosenCampaigns = campaignsList.campaigns.filter(
-			(campagin) =>
-				campagin.settings.title === title && campagin.status === "sent"
-		);
-		if (!chosenCampaigns || chosenCampaigns.length === 0) {
-			next(new ErrorResponse("Campaign not found", 301));
-		}
-
-		try {
-			const { id } = chosenCampaigns[0];
-			const { report_summary } = await MailchimpMarketingModel.campaigns.get(
-				id
-			);
-			res.json({ report_summary }).status(200);
-		} catch (e) {
-			next(new ErrorResponse("server error", 500));
-		}
-	} catch (e) {
-		next(new ErrorResponse("No campagin found", 301));
-	}
-};
-
-const getDataFromMailchimp = async (req, res, next) => {
-	try {
-		console.log(req.body);
-		// const dbCampagin = await campaginModel.findById(req.body.id);
-		// const mailChimpCampagins = await MailchimpMarketingModel.campaigns.list();
-		// if (dbCampagin) {
-		// 	mailChimpCampagins.map((campagin) => {
-		// 		return {
-		// 			archive_url: campagin.archive_url,
-		// 			id: campagin.settings.title,
-		// 			send_time: campagin.send_time,
-		// 		};
-		// 	});
-		// 	const updatedList = await campaginModel.insertMany(mailChimpCampagins);
-		// 	res.json({ updatedList }).status(200);
-		// }
-		// const { send_time, id, archive_url } = req.body;
-		// const newCampagin = await campaginModel.insertMany({
-		// 	send_time,
-		// 	id,
-		// 	archive_url,
-		// });
-	} catch (e) {
-		// console.log(e);
-		res.send("error");
-	}
-};
-
 module.exports = {
 	getCampaignsSorted,
-	getCampaignByTitle,
-	getDataFromMailchimp,
 };
