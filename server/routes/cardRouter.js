3ea424f0d1e24e970c1bcf940b00d1b2025f63ba
Asalef <asalef10@gmail.com>
2021-10-03 18:30:29 +0300

    update commens

--- server/routes/cardRouter.js
+++ server/routes/cardRouter.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const cardRouter = express.Router()
 const cardController = require('../controllers/cardController')
-cardRouter.get('/',cardController.getAllCard)
-cardRouter.post('/sendEmail',cardController.sendEmailCard)
+
+// @ GET request to get all cards.
+// @ URL http://localhost:5000/card
+cardRouter.get('/', cardController.getAllCard)
+
+// @ POST request to send memberShip to mail.
+// @ URL http://localhost:5000/card/sendEmail
+cardRouter.post('/sendEmail', cardController.sendEmailCard)
+
+// @ POST request to get  memberShip by email.
+// @ URL http://localhost:5000/card/getCardByEmail
 cardRouter.post('/getByEmail',cardController.getCardByEmail)
 module.exports = cardRouter 
--- server/routes/memberRouter.js
+++ server/routes/memberRouter.js
@@ -1,25 +1,25 @@
-const express = require("express");
-const memberController = require("../controllers/memberController");
-const mailChimpController = require("../controllers/mailChimpController");
+const express = require('express');
+const memberController = require('../controllers/memberController');
+const mailChimpController = require('../controllers/mailChimpController');
 
 const memberRouter = express.Router();
 
-memberRouter.get("/getMembers", memberController.getMembers);
+memberRouter.get('/getMembers', memberController.getMembers);
 memberRouter.post(
-	"/createMember",
-	memberController.createMember
-	// mailChimpController.createMember
+  '/createMember',
+  memberController.createMember
+  // mailChimpController.createMember
 );
 memberRouter.put(
-	"/updateMember/:Email",
-	memberController.updateMember
-	// mailChimpController.updateMember
+  '/updateMember/:Email',
+  memberController.updateMember
+  // mailChimpController.updateMember
 );
 
 memberRouter.delete(
-	"/deleteMember/:Email",
-	memberController.deleteMember
-	// mailChimpController.deleteMember
+  '/deleteMember/:Email',
+  memberController.deleteMember
+  // mailChimpController.deleteMember
 );
 
 module.exports = memberRouter;
