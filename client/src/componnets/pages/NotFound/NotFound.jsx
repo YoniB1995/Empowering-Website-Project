8aa754c47903619f81317dff584767167b9d2ecc
YoniB1995 <yonatansamfisher@gmail.com>
2021-10-03 19:53:53 +0300

    updated not found page and cleaner code

--- client/src/componnets/pages/NotFound/NotFound.jsx
+++ client/src/componnets/pages/NotFound/NotFound.jsx
@@ -5,12 +5,24 @@ import "./NotFound.css";
 const NotFound = () => {
   return (
     <div className="not-found">
-      <h1>404 Error , Page Not Found!</h1>
-      <img
-        src="https://scontent.fhfa4-1.fna.fbcdn.net/v/t1.6435-9/129759444_104200958220746_6720961362101384791_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=e3f864&_nc_ohc=xIYzthtFmeAAX9CreIi&_nc_ht=scontent.fhfa4-1.fna&oh=c98a72bb61708c1e1e24535ecd7fc679&oe=617ED2C0"
-        alt="empowering-women-logo"
-      />
-      <Link to="/">בחזרה לדף הבית</Link>
+      <div className="vertical-card">
+        <div className="vertical-card-img-container">
+          <img src="./empowering_logo2.png" alt="vision-vector" id="img-card" />
+        </div>
+        <div className="vertical-card-body">
+          <div className="header">
+            <div className="header-decoration"></div>
+            <h3 className="vertical-card-title" style={{ color: "black" }}>
+              שגיאה 404{" "}
+            </h3>
+          </div>
+          <div className="vertical-card-description">
+            {" "}
+            <div>הקישור לא זמין / לא קיים. </div>
+            <Link to="/">בחזרה לדף הבית</Link>{" "}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
