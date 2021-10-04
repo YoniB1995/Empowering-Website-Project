fcac8054b22648e7e99d8a6f4b62900e168ba838
Lior3796 <liorlsa9@gmail.com>
2021-10-04 16:23:49 +0300

    fix newsletter

--- client/src/componnets/pages/Newsletter/Newsletter.js
+++ client/src/componnets/pages/Newsletter/Newsletter.js
@@ -1,20 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { getAllCampagins } from "../../../service/newsletter-service";
 import { ArrowDownOutlined } from "@ant-design/icons";
+
 import './Newsletter.css';
 
 const Newsletter = () => {
-  const [campagins1, setCampagins] = useState({});
+  const [campagins1, setCampagins] = useState([]);
 
-  const getCampagins = () => {
+
+  useEffect(() => {
     getAllCampagins().then(res => res.json())
       .then(res => setCampagins(res.sortedCampagins))
       .catch(err => console.log(err))
-  }
-  useEffect(() => {
-    getCampagins();
   }, [])
-  console.log(campagins1);
+  // console.log(campagins1);
 
   const [visible, setVisible] = useState(false);
   const [iframeSelected, setIframeSelected] = useState({});
@@ -25,17 +24,15 @@ const Newsletter = () => {
   };
   return (
     <>
+      <h1>dsads</h1>
       <div className="campaign-T">
-        {campagins1.map((campaign) => {
-          return (
-            <>
-              <h3 onClick={() => showIframe(campaign.id)}>
-                ניזולטר
-                <ArrowDownOutlined style={{ background: "white", width: "30px", height: "30px", borderRadius: "100%" }} />
-              </h3>
-            </>
-          );
-        })}
+        {campagins1.map((campaign) =>
+
+          <h3 onClick={() => showIframe(campaign.id)}>
+            ניזולטר
+            <ArrowDownOutlined style={{ background: "white", width: "30px", height: "30px", borderRadius: "100%" }} />
+          </h3>
+        )}
       </div>
       {
         visible && (
